8bba79480fbb35f3eebc3b6b55572cfb99de6812
huangkf <huangkf@16b3574f-0b8b-453d-8967-36fd7926bc26>
2008-11-28 17:34:13 +0000

    Enabled guarded edges, still need to enable the ability to edit guarded edges and generate guarded edges
    git-svn-id: svn+ssh://www.cas.mcmaster.ca/nfs/u0/grad/huangkf/repo/ver.svn/trunk@14 16b3574f-0b8b-453d-8967-36fd7926bc26

--- code/plcstatecharteditor/src/plcstatecharteditor/DirectedLineConnectionFigure.java
+++ code/plcstatecharteditor/src/plcstatecharteditor/DirectedLineConnectionFigure.java
@@ -14,6 +14,7 @@ import org.jhotdraw.draw.*;
  */
 public class DirectedLineConnectionFigure extends LabeledLineConnectionFigure implements ConnectionFigure, Typed {
     private LineDecoration decoration;
+    private DirectedLineConnectionModel accociatedcode = new DirectedLineConnectionModel();
     
     DirectedLineConnectionFigure()
     {
@@ -21,6 +22,8 @@ public class DirectedLineConnectionFigure extends LabeledLineConnectionFigure im
         decoration = new ArrowTip(Math.PI / 11, 13, 0, false, true, true); //prepare our arrow tip
         
         this.setAttribute(AttributeKeys.END_DECORATION, decoration); //make our graph object directed
+
+        update();
     }
 
     @Override
@@ -37,10 +40,21 @@ public class DirectedLineConnectionFigure extends LabeledLineConnectionFigure im
     public String getTypeString() {
         return "GOTO";
     }
+
+    private void update()
+    {
+        this.removeAllChildren();
+        addTextLabel(accociatedcode.getDisplayLabel());
+        this.invalidate();
+    }
     
-    private void addTextBox()
+    private void addTextLabel(String text)
     {
         this.setLayouter(new LocatorLayouter());
+        TextFigure label = new TextFigure(text);
+        LocatorLayouter.LAYOUT_LOCATOR.set(label, new BezierLabelLocator(0.3, -Math.PI / 4, 10.0));
+        //TODO: add logic to hook onto a text change event so we can update the fields
+        this.add(label);
     }
 }
     
--- code/plcstatecharteditor/src/plcstatecharteditor/SpecialAttributeKeys.java
+++ code/plcstatecharteditor/src/plcstatecharteditor/SpecialAttributeKeys.java
@@ -12,7 +12,11 @@ import org.jhotdraw.draw.AttributeKey;
  * @author huangkf
  */
 public class SpecialAttributeKeys {
+    /*Used in store blocks*/
     public static final AttributeKey<StoreBlock.storeobj> MODIFY_LINK_ID = new AttributeKey("MODIFY_LINK_ID");
     public static final AttributeKey<StoreBlock.storeobj> MODIFY_LINK_VAL = new AttributeKey("MODIFY_LINK_VAL");
     public static final AttributeKey<CodeVarType> MODIFY_LINK_TYPE = new AttributeKey("MODIFY_LINK_TYPE");
+    
+    /*Used in transitions*/
+    public static final AttributeKey<CodeVarType> MODIFY_LINK_TRANSITION = new AttributeKey("MODIFY_LINK_TRANSITION");
 }
