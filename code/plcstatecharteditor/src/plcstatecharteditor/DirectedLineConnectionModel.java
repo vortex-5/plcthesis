d4046d344fba0b5b6d4a8849e35db7d269119de6
huangkf <huangkf@16b3574f-0b8b-453d-8967-36fd7926bc26>
2008-11-28 17:33:33 +0000

    Added the ability to generate declarations with sanity checks.
    Next task will be to enable guarded edges.
    
    git-svn-id: svn+ssh://www.cas.mcmaster.ca/nfs/u0/grad/huangkf/repo/ver.svn/trunk@13 16b3574f-0b8b-453d-8967-36fd7926bc26

---
+++ code/plcstatecharteditor/src/plcstatecharteditor/DirectedLineConnectionModel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package plcstatecharteditor;
+
+/**
+ *
+ * @author huangkf
+ */
+public class DirectedLineConnectionModel extends CodeBlock{
+    String guardCondition = "true";
+
+    public DirectedLineConnectionModel() {
+    }
+
+    @Override
+    public String getCode() {
+        return "if (" + guardCondition + ")";
+    }
+
+    public String getDisplayLabel() {
+        return guardCondition;
+    }
+
+    public void inputFix() {
+        if (guardCondition.equals(""))
+        {
+            guardCondition = "true";
+        }
+    }
+
+    @Override
+    public String getTypeString() {
+        return "Transition";
+    }
+
+    public void setNewGuardCondition(String text) {
+        guardCondition = text;
+    }
+}
